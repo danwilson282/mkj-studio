21249892cb13ed680b41717595557ff51ead2ab3
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-12 13:42:19 +0100

    require login flag component working

--- schema/components/RequiresLoginInput.tsx
+++ schema/components/RequiresLoginInput.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Stack, Switch, Text } from '@sanity/ui'
+import { Stack, Switch, Text, Flex, Box, Label, Card } from '@sanity/ui'
 import { set, unset } from 'sanity'
 import { useClient } from 'sanity'
 import { BooleanInputProps, useFormValue } from 'sanity';
@@ -9,46 +9,55 @@ type Reference = {
   _type?: string; // optional, usually the document type
 };
 export default function RequiresLoginInput(props: BooleanInputProps) {
-  const { value, onChange } = props
+  const { value, onChange, readOnly } = props
   const client = useClient({ apiVersion: process.env.SANITY_STUDIO_API_VERSION || '2023-10-10' })
   const parent = useFormValue(['parent']) as Reference;
   const [parentRequiresLogin, setParentRequiresLogin] = useState(false)
 
   useEffect(() => {
     const fetchParent = async () => {
-        
       const parentRef = parent?._ref
       if (!parentRef) return
 
       const data = await client.fetch(`*[_id == $id][0]{requiresLogin}`, {
         id: parentRef,
       })
+
       if (data?.requiresLogin) {
         setParentRequiresLogin(true)
-        onChange(set(true)) // force true if parent requires login
+
+        // Only patch if not read-only and not already true
+        if (!readOnly && value !== true) {
+          onChange(set(true))
+        }
       }
     }
 
     fetchParent()
-  }, [parent, onChange, client])
+  }, [parent?._ref, readOnly, value, onChange])
 
   return (
-    <Stack space={2}>
-        <Text size={1}>
-          Requires login?
-        </Text>
-      <Switch
-        checked={!!value}
-        onChange={(e) => {
-        const target = e.target as HTMLInputElement;
-        onChange(target.checked ? set(true) : unset());
-        }}
-        disabled={parentRequiresLogin}
-      />
+        <Stack space={3}>
+      <Flex align="center">
+        <Box flex={1}>
+          <Label>Requires login</Label>
+        </Box>
+        {!parentRequiresLogin && <Switch
+          checked={!!value}
+          onChange={(e) => {
+            const target = e.target as HTMLInputElement;
+            onChange(target.checked ? set(true) : unset());
+          }}
+          disabled={parentRequiresLogin}
+        />}
+      </Flex>
+
       {parentRequiresLogin && (
-        <Text size={1} muted>
-          This page must require login because the parent does.
-        </Text>
+        <Card padding={2} tone="transparent">
+          <Text size={1} muted>
+            This page must require login because the parent does.
+          </Text>
+        </Card>
       )}
     </Stack>
   )
