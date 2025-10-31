f14cd718e161bc6595e1e79b2a28e9afc3e51150
Dan Wilson <danwilson282@yahoo.co.uk>
2025-10-31 20:43:40 +0000

    added icon picker

--- schema/sections/index.ts
+++ schema/sections/index.ts
@@ -1,8 +1,10 @@
 import hero from './hero';
 import textBlock from './textBlock';
 import imageBlock from './imageBlock';
+import icon from './icon';
 export default [
   hero,
+  icon,
   textBlock,
   imageBlock
 ];
